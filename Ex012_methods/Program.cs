4761b79572c15da1df1169dd9262b3a6bbe678d6
anna.adjikiss <adjikiss@mail.ru>
2022-03-22 11:02:29 +0300

    ex012 metods

---
+++ Ex012_methods/Program.cs
@@ -0,0 +1,178 @@
+﻿// Вид 1
+void Metod1()
+{
+    Console.WriteLine("Автор ...");
+}
+//Metod1();
+
+
+// Вид 2
+void Metod2 (string msg)
+{
+    Console.WriteLine(msg);
+}
+//Metod2("Текст сообщения");
+
+void Metod21 (string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+// Metod21("Текст", 4);
+// Metod21(msg: "Текст", count: 4);  !!! можно прям переменные обозначать конкретно при вызове функции
+// и тогда можно даже в другом порядке
+//Metod21(count: 4, msg: "новый текст!");
+
+//Вид 3
+int Metod3()
+{
+    return DateTime.Now.Year;
+}
+int year = Metod3();
+//Console.WriteLine(year);
+
+
+//Вид 4
+/*
+string Metod4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Metod4(10, "asdf");
+Console.WriteLine(res);
+*/
+
+//Цикл For
+/*
+string Metod4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Metod4(10, "<>");
+Console.WriteLine(res);
+*/
+
+//Цикл в цикле (пример - таблица умножения)
+/*
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+*/
+
+//=============Задача "Работа с текстом"
+
+// Дан текст. В тексте все пробелы нужно заменть черточками
+// маленькие буквы "к" заменить большими буквами "К"
+// а большие "С" заменить маленькими "с"
+/*
+string text = "-Я думаю, - сказал князь, улыбаяс, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+*/
+
+//===========Задача на сортировку массива - от мин к макс
+/*
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+*/
+
+//===========Задача на сортировку массива - от макс к мин
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+//PrintArray(arr);
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
